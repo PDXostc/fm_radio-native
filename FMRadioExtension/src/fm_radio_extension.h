c3dce34554ab2f521bf27d0f880125d183b9c585
Frederic Plourde <frederic.plourde@collabora.co.uk>
2015-01-29 22:08:11 -0500

    EXT: harmonized coding style

--- FMRadioExtension/src/fm_radio_extension.h
+++ FMRadioExtension/src/fm_radio_extension.h
@@ -8,13 +8,13 @@
 #include "../../extension_common/extension.h"
 
 class FMRadioExtension : public common::Extension {
- public:
-  FMRadioExtension();
-  virtual ~FMRadioExtension();
+    public:
+        FMRadioExtension();
+        virtual ~FMRadioExtension();
 
- private:
-  // common::Extension implementation
-  virtual common::Instance* CreateInstance();
+    private:
+    // common::Extension implementation
+    virtual common::Instance* CreateInstance();
 };
 
 #endif  // FM_RADIO_EXTENSION_H_
