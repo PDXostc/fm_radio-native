9656217a7eceaf29f163a12c4c988fc8eb3c11a2
Art McGee <amcgee7@jaguarlandrover.com>
2015-02-27 11:18:21 -0800

    connect volume control to tizen
    Change-Id: I86f307adf9463f7720264ca9c67ae92ffa4fb9b7

--- DNA_common/components/volume/js/volume.js
+++ DNA_common/components/volume/js/volume.js
@@ -1,6 +1,79 @@
 console.log("start of volume.js");
 var volumeSettingsPage={};
 volumeSettingsPage.TemplateHTML = "DNA_common/components/volume/volume.html";
+// Holds the Audio context for change audio (volumb and ballance in varous domains)
+volumeSettingsPage.audioContext = null;
+volumeSettingsPage.indexVolumeDomain = 1; // this should change when the domain of control changes.
+volumeSettingsPage.indexMuteDomain = 2; // this should change when the domain of control changes.
+
+// Lists all the audio domains
+volumeSettingsPage.print_all_controls = function(context) {
+	console.info("print_all_controls context:",context);
+    for (var i = 0; i < context.volume_controls.length; i++)
+        console.info("audio domain:",context.volume_controls[i].label,"index",i);
+}
+
+// Called when a connect to the audio context
+volumeSettingsPage.connect_success = function (context) {
+	volumeSettingsPage.audioContext = context;
+    console.info("Connected to the audio system.");
+    volumeSettingsPage.print_all_controls(volumeSettingsPage.audioContext);
+}
+
+// Called when a connect failes
+volumeSettingsPage.connect_error = function (error) {
+    console.info("Failed to connect to the audio system: " + error.message);
+}
+
+// Called when a connection is removed
+volumeSettingsPage.disconnected = function (event) {
+	volumeSettingsPage.audioContext = null;
+    console.info("Connection to the audio system lost.");
+}
+
+/* Called to set the master volume
+ * Note the index of indexVolumeDomain is selected as the default for controlling volume
+ * This may need to change if there are changes in the audio domains
+ * In the future it would be good to select the volume domain under control
+ */
+volumeSettingsPage.setMasterVolumb = function(vol) {
+	if (volumeSettingsPage.audioContext) {
+		volumeSettingsPage.audioContext.volume_controls[volumeSettingsPage.indexVolumeDomain].set_volume(vol);
+	}
+}
+
+// Called to get the master volume note that this also has the index.
+volumeSettingsPage.getMasterVolumb = function() {
+	if (volumeSettingsPage.audioContext) {
+		return volumeSettingsPage.audioContext.volume_controls[volumeSettingsPage.indexVolumeDomain].volume;
+	} else {
+		return 0;
+	}
+}
+
+/* Called to set the master mute
+ * Note the index of indexMuteDomain is selected as the default for controlling volume
+ * This may need to change if there are changes in the audio domains
+ * In the future it would be good to select the volume domain under control
+ */
+volumeSettingsPage.setMasterMute = function(mute) {
+	if (volumeSettingsPage.audioContext) {
+		volumeSettingsPage.audioContext.mute_controls[volumeSettingsPage.indexMuteDomain].set_muted(mute);
+	}
+}
+
+// Called to get the master volume note that this also has the index.
+volumeSettingsPage.getMasterMute = function() {
+	if (volumeSettingsPage.audioContext) {
+		return volumeSettingsPage.audioContext.mute_controls[volumeSettingsPage.indexMuteDomain].muted;
+	} else {
+		return false;
+	}
+}
+
+
+window.tizen.audiosystem.connect(volumeSettingsPage.connect_success, volumeSettingsPage.connect_error);
+window.tizen.audiosystem.addEventListener("disconnected", volumeSettingsPage.disconnected);
 
 volumeSettingsPage.ShowPage = function() { 
 		console.log('volume page show_click();');
@@ -9,154 +82,19 @@ volumeSettingsPage.ShowPage = function() {
 
 				/* ==== ==== ==== volume slider js code ==== ==== ==== */
 
-		var settingsSlide=[];
-		//var homescreenTimeout;
-		//var BottomBar = {};
-		//BottomBar.TemplateHTML = "DNA_common/components/bottomBar/bottomBar.html";
-
-/*		BottomBar.LogoTimeoutMouseDown = function (e){
-				console.log("BottomBar.LogoTimeoutMouseDown()");
-					homescreenTimeout = setTimeout(function() {
-						clearTimeout(homescreenTimeout);
-						if(tizen.application.getCurrentApplication().appInfo.packageId != "JLRPOCX001"){
-							tizen.application.getCurrentApplication().exit();
-						}
-					}, 2500);
-				}
-				
-		BottomBar.LogoTimeoutMouseUp = function (e){
-					clearTimeout(homescreenTimeout);
-				}*/
-				
-/*		BottomBar.pageUpdate = function () {
-				$('#bottomBar').replaceWith(BottomBar.bottomBarHTML.valueOf());
-				
-				$("#bottomBarLogoImg").mousedown(BottomBar.LogoTimeoutPress);
-				
-				$("#bottomBarLogoImg").mouseup(BottomBar.LogoTimeoutMouseUp);
-
-				$("#volumeIndicator").click(function (e){
-					$("#settings-volumeSlider").toggle();
-				});
-				/* ==== ==== ==== init volume slider touch events ==== ==== ==== */
-/*
-				$("#settings-volumeSlider").on('mousedown',volDown);
-				$("#settings-volumeSlider").on('mouseout',volOut);
-				$("#settings-volumeSlider").on('mouseover',volOver);
-				$("#settings-volumeSlider").on('mousemove',volMove);
-				$("#settings-volumeSlider").on('touchstart',volDown);
-				$("#settings-volumeSlider").on('touchleave',volOut);
-				$("#settings-volumeSlider").on('touchend',volOut);
-				$("#settings-volumeSlider").on('touchmove',volMove);
-
-				$("body").mouseup(function(e){
-					Slide.mousedown=0; Slide.button=0;
-				});
-			}*/
-
-/*		BottomBar.includeHTMLSucess = function(linkobj) {
-				console.log("BottomBar.includeHTMLSucess()");
-				BottomBar.import = linkobj.path[0].import;
-				console.log(BottomBar.import);
-				BottomBar.bottomBarHTML = BottomBar.import.getElementById('bottomBar');
-				setTimeout(BottomBar.pageUpdate,2000);
-			}
-
-		BottomBar.includeHTMLFailed = function(linkobj) {
-			console.log("load bottomBar.html failed");
-			console.log(linkobj);
-		};
-			
-		includeHTML(BottomBar.TemplateHTML, BottomBar.includeHTMLSucess, BottomBar.includeHTMLFailed);*/
-
-		/* ==== ==== ==== init volume slider js code ==== ==== ==== */
-
-		function volDown(e){
-			console.log("voldown 1");
-			settingsSlide.mousedown=1; settingsSlide.button=1;
-			volMove(e);
-		}
-		function volOut(){
-			console.log("volout 0");
-			settingsSlide.mousedown=0;
-		}
-		function volOver(){
-			if(settingSlide.button)
-				settingSlide.mousedown=1;
-			console.log("volover "+settingSlide.mousedown);
-		}
-		function volMove(e){
-			var height=e.target.offsetHeight;
-			var yloc=e.pageY || e.originalEvent.targetTouches[0].clientY;
-			console.log("mouse move "+height+" "+yloc);
-			console.warn(e);
-			if(settingSlide.mousedown){
-				//TODO: replace e.target.offsetHeight and e.pageY for touch events!
-				
-				//pull some coordinates and percentages
-				var parentOffset = height-120; //seems to be offset by topbar height
-				var relY = Math.floor((yloc - parentOffset)/8.96);
-				
-				//quick and dirty math
-				if(relY<1) relY=1;
-				if(relY>100) relY=100;
-				var invY = 100-relY;
-				jqY = Math.floor((invY+1)*0.92+163); //From 163 to 255 = 92 different discreet volume settings
-				
-				//update appropriate onscreen widgets
-				$("#volumeCrop").width(invY+'%');
-				$("#settingsVolumeSlideCrop").height(invY+'.1%');
-				$("#settings-volumeKnob").css('top',(relY*8.80-70)+'px');
-				
-				//encode some stringified json (jqY = level 163 to 255) and send to most
-				var jsonenc = {"api":"setTone","dest":"volume","level":jqY,"incr":0};
-				most.mostAsync(JSON.stringify(jsonenc), volumeQueryCB);
-			}
-		}
 		// Volume control update timer; this keeps the volume control slider synchronized
 		// when moving from widget to widget.
 
 		var volumeTimer = setInterval(refreshVolume, 2000);
-		var previousVolume = -1, curVolume=0;
 
 		// This is called by a periodic timer to cause a volumeQuery command to be sent to MOST. This is done so that when
 		// navigating from screen to screen, the volume control slider on the visible screen will stay in synch with the
 		// current MOST volume setting.
 		//
-		var volLogCnt=0;
 
 		function refreshVolume() {
-										
-			var jsonenc = {api:"setTone", dest:"volumeQuery", level:0, incr:0};
-			
-			volLogCnt++;
-			if(volLogCnt == 5)
-			{
-				console.log("MOSTLOG refreshVolume query");
-				volLogCnt=0;
-			}
-			most.mostAsync(JSON.stringify(jsonenc), volumeQueryCB);
-			
+			volumeSettingsPage.UpdateVolumeSlider(volumeSettingsPage.getMasterVolumb());
 		}
-		// Sets the variable which holds the latest updated volume
-		// received from the MOST extension.
-		var volLogCntCB=0;
-
-		var volumeQueryCB = function(response) {
-
-			volLogCnt++;
-			if(volLogCntCB == 5)
-			{	
-				 console.log("MOSTLOG: volumeQueryCB " + response);
-				 volLogCntCB=0;
-			}
-			curVolume = response;
-			var sl = (curVolume - 159)/4;
-				
-			$(".noVolumeSlider").val(sl);
-		};
-
-
 	};
 
 volumeSettingsPage.HidePage = function() { 
@@ -165,21 +103,51 @@ volumeSettingsPage.HidePage = function() {
 		$('#volumePage').addClass('hidden');
 	};
 
+volumeSettingsPage.OnVolumeSliderMinus = function() {
+	console.log("OnVolumeSliderMinus");
+	var vol = volumeSettingsPage.getMasterVolumb();
+	if (vol>.05) {
+		vol = vol - .05;
+	} else {
+		vol = 0;
+	}
+	volumeSettingsPage.setMasterVolumb(vol);
+	volumeSettingsPage.UpdateVolumeSlider(vol);
+}
+volumeSettingsPage.OnVolumeSliderPlus = function() {
+	console.log("OnVolumeSliderPlus");
+	var vol = volumeSettingsPage.getMasterVolumb();
+	if (vol<.95) {
+		vol = vol + .05;
+	} else {
+		vol = 1;
+	}
+	volumeSettingsPage.setMasterVolumb(vol);
+	volumeSettingsPage.UpdateVolumeSlider(vol);
+}
+
+volumeSettingsPage.onSliderChange = function() {
+	var vol = $("#settingsVolumeSlider").attr("value") / 100;
+	console.log("onSliderChange",vol,$("#settingsVolumeSlider").attr("value"));
+	volumeSettingsPage.setMasterVolumb(vol);
+}
+
+volumeSettingsPage.UpdateVolumeSlider = function(vol) {
+	$("#settingsVolumeSlider").attr("value",vol*100);
+}
+
+volumeSettingsPage.onMuteClick = function() {
+	if ($("#volumePowerButton").children("div").hasClass("on")) {
+		$("#volumePowerButton").children("div").removeClass("on").addClass("off");
+		volumeSettingsPage.setMasterMute(true);
+	} else {
+		$("#volumePowerButton").children("div").removeClass("off").addClass("on");
+		volumeSettingsPage.setMasterMute(false);
+	}
+}
+
 volumeSettingsPage.pageUpdate = function() {
 	console.log("volume pageUpdate()");
-
-	$("#settingsVolumeSlider").on('mousedown',volDown);
-	$("#settingsVolumeSlider").on('mouseout',volOut);
-	$("#settingsVolumeSlider").on('mouseover',volOver);
-	$("#settingsVolumeSlider").on('mousemove',volMove);
-	$("#settingsVolumeSlider").on('touchstart',volDown);
-	$("#settingsVolumeSlider").on('touchleave',volOut);
-	$("#settingsVolumeSlider").on('touchend',volOut);
-	$("#settingsVolumeSlider").on('touchmove',volMove);
-
-	$("body").mouseup(function(e){
-		settingSlide.mousedown=0; settingSlide.button=0;
-	});
 
 	if (!$('#settingsPage').length) {
 		setTimeout(volumeSettingsPage.pageUpdate,1000);
@@ -187,7 +155,16 @@ volumeSettingsPage.pageUpdate = function() {
 	else {
 		$("#settingsPage").append(volumeSettingsPage.import.getElementById('volumePage'));
 		Settings.addUpdateSettingsPage('volume','settings',volumeSettingsPage.ShowPage);
-		var close_button = document.getElementById('volumeBackArrow').onclick = volumeSettingsPage.HidePage;
+		$("#volumePowerButton").on("click",volumeSettingsPage.onMuteClick);
+		$("#volumePowerButton").children("div").removeClass("off").addClass("on");
+		$("#settingsVolumeSliderMinus").on('click',volumeSettingsPage.OnVolumeSliderMinus);
+		$("#settingsVolumeSliderPlus").on('click',volumeSettingsPage.OnVolumeSliderPlus);
+		$("#settingsVolumeSlider").attr("max",100);
+		$("#settingsVolumeSlider").attr("min",0);
+		$("#settingsVolumeSlider").on('change',volumeSettingsPage.onSliderChange);
+
+		document.getElementById('volumeBackArrow').onclick = volumeSettingsPage.HidePage;
+		volumeSettingsPage.UpdateVolumeSlider(volumeSettingsPage.getMasterVolumb());
 	}
 };
 
@@ -197,7 +174,6 @@ volumeSettingsPage.includeHTMLSucess = function(linkobj) {
    volumeSettingsPage.volumePageHTML = volumeSettingsPage.import.getElementById('volumePage');
    volumeSettingsPage.volumeDeviceHTML = volumeSettingsPage.import.getElementById('volumeDeviceTemplate');   
    onDepenancy("Settings.settingsPage",volumeSettingsPage.pageUpdate,"Bluetooth");
-   //volumeSettingsPage.pageUpdate();
 };
 
 volumeSettingsPage.includeHTMLFailed = function(linkobj) {
